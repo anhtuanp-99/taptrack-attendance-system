9cfaf56f0b66e19b65ed8f79fdd1c24c9fb94c58
anhtuanp-99 <anhtuanhl2004@gmail.com>
2026-08-27 15:07:21 +0700

    Initial commit for TapTrack project

---
+++ src/main/java/com/taptrack/TapTrackApplication.java
@@ -0,0 +1,13 @@
+package com.taptrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TapTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taptrack/TapTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taptrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
